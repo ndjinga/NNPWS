3bc83156dd81f21a3425d686cd97539e210a4b8e
NDJINGA Michael 206165 <mn206165@is245489.intra.cea.fr>
2025-11-21 16:35:28 +0100

    Added blank cxx file

---
+++ src/NNPWS.cxx
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Class definition
+class GfG
+{
+public:
+    // Data member
+    int val;
+
+    // Member function
+    void show()
+    {
+        cout << "Value: " << val << endl;
+    }
+
+    int compute_d_h_d_p_T_pT(double p, double T, int& r) {
+        return 0;
+    }
+};
+
+int main()
+{
+
+    // Object will declareed here
+    return 0;
+}
