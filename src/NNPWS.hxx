5c7aae215a63ec9d744a112b635cd7bcac9fbd75
NDJINGA Michael 206165 <mn206165@is245489.intra.cea.fr>
2025-11-21 16:42:53 +0100

    Updating .hxx file

--- src/NNPWS.hxx
+++ src/NNPWS.hxx
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2023, CEA
+* Copyright (c) 2025, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -15,8 +15,8 @@
 
 
 
-#ifndef __EOS_CoolProp_HH__
-#define __EOS_CoolProp_HH__
+#ifndef __NNPWS_HH__
+#define __NNPWS_HH__
 
 #include <fstream>
 #include <algorithm>
@@ -25,91 +25,35 @@
 using std::istringstream ;
 using std::ostringstream ;
 
-#include "EOS/API/EOS.hxx"
-#include "EOS/API/EOS_Fluid.hxx"
-#include "EOS/API/EOS_Field.hxx"
-#include "EOS/API/EOS_Fields.hxx"
-#include "EOS/API/EOS_Error.hxx"
-#include "EOS/API/EOS_Error_Field.hxx"
-#include "EOS/API/EOS_Config.hxx"
-#include "EOS/API/EOS_eosdatadir.hxx"
 #include "Language/API/RegisterType.hxx"
 #include "Language/API/Types_Info.hxx"
 #include "system/arch.h"
 
-using namespace NEPTUNE ;
-
-namespace NEPTUNE_EOS
-{
-
-  //! EOS_CoolProp class
-  class EOS_CoolProp : public EOS_Fluid
+  //! NNPWS class
+  class NNPWS : public EOS_Fluid
   {
-    static const AString TMStr    ;
-    static const AString MVStr    ;
+    static const string TMStr    ;
+    static const string MVStr    ;
 
   public:
     //! Constructor method
-    EOS_CoolProp();
+    NNPWS();
 
 //    //! Copy constructor, which should not be used
-//    EOS_CoolProp(const EOS_CoolProp&);
+//    NNPWS(const NNPWS&);
 
     //! Destructor method
-    ~EOS_CoolProp();
+    ~NNPWS();
 
     //! Getter methods
-    const AString& table_name()   const ;
-    const AString& version_name() const ;
-    const AString& phase_name()   const ;
+    const string& table_name()   const ;
+    const string& version_name() const ;
+    const string& phase_name()   const ;
 
     //! for initialisation :
     //
-    const AString& fluid_name() const;
-    const AString& equation_name() const;
-
-    //! initializers
-    int init(const Strings&);
-    int init(const Strings&, const Strings&);
-    int init(const Strings&, const Strings&, const double, const double, const double, const double) ;
-
-    //! Compute calculations from fields
-    /*!
-     * \param[in] in input field. Currently supported are : P
-     * \param[in,out] out output field array. All fields must have the correct size.
-     * \param[in,out] err error field. Must have the correct size.
-     * \return worst error found in error field
-     */
-//    EOS_Error compute(const EOS_Field& in, EOS_Fields& out, EOS_Error_Field& err) const;
-
-    //! Compute calculations from fields
-    /*!
-     * \param[in] in input field. Currently supported are : P, h
-     * \param[in,out] out output field . Must have the correct size.
-     * \param[in,out] err error field. Must have the correct size.
-     * \return worst error found in error field
-     */
-//    EOS_Error compute(const EOS_Field& in, EOS_Field& out, EOS_Error_Field& err) const;
-
-    //! Compute calculations from fields
-    /*!
-     * \param[in] in1 input field. Currently supported are : P, h, T
-     * \param[in] in2 input field. Currently supported are : P, h, T
-     * \param[in,out] out output field array. All fields must have the correct size.
-     * \param[in,out] err error field. Must have the correct size.
-     * \return worst error found in error field
-     */
-//    EOS_Error compute(const EOS_Field& in1, const EOS_Field& in2, EOS_Fields& out, EOS_Error_Field& err) const;
-
-    //! Compute calculations from fields
-    /*!
-     * \param[in] in1 input field. Currently supported are : P, h, T
-     * \param[in] in2 input field. Currently supported are : P, h, T
-     * \param[in,out] out output field . Must have the correct size.
-     * \param[in,out] err error field. Must have the correct size.
-     * \return worst error found in error field
-     */
-//    EOS_Error compute(const EOS_Field& in1, const EOS_Field& in2, EOS_Field& out, EOS_Error_Field& err) const;
+    const string& fluid_name() const;
+    const string& equation_name() const;
 
     //! Global methods
     EOS_Error compute_Ph(const char* const property_name, double in1, double in2, double& out) const ;
@@ -207,62 +151,62 @@ namespace NEPTUNE_EOS
     EOS_Internal_Error compute_pr_pT(double p, double T, double& pr) const;
 
     //General fluid properties
-    virtual EOS_Internal_Error get_T_min(double&) const;
-    virtual EOS_Internal_Error get_T_max(double&) const;
-    virtual EOS_Internal_Error get_p_max(double&) const;
-    virtual EOS_Internal_Error get_p_min(double&) const;
-    virtual EOS_Internal_Error get_h_min(double&) const;
-    virtual EOS_Internal_Error get_h_max(double&) const;
+    EOS_Internal_Error get_T_min(double&) const;
+    EOS_Internal_Error get_T_max(double&) const;
+    EOS_Internal_Error get_p_max(double&) const;
+    EOS_Internal_Error get_p_min(double&) const;
+    EOS_Internal_Error get_h_min(double&) const;
+    EOS_Internal_Error get_h_max(double&) const;
 
-    virtual EOS_Internal_Error get_T_crit(double&) const;
-    virtual EOS_Internal_Error get_p_crit(double&) const;
-    virtual EOS_Internal_Error get_h_crit(double&) const;
+    EOS_Internal_Error get_T_crit(double&) const;
+    EOS_Internal_Error get_p_crit(double&) const;
+    EOS_Internal_Error get_h_crit(double&) const;
 
     /* Get molar mass in kg/mol */
-    virtual EOS_Internal_Error get_mm(double&) const;
+    EOS_Internal_Error get_mm(double&) const;
 
     /* Partial derivatives (P,H) */
-    virtual EOS_Internal_Error compute_d_T_d_p_h_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_T_d_h_p_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_T_d_p_h_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_T_d_h_p_ph(double p, double h, double& r) const;
 
     /*
      * Not used since too "touchy"
-    virtual EOS_Internal_Error compute_d_rho_d_p_h_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_rho_d_h_p_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_u_d_p_h_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_u_d_h_p_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_s_d_p_h_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_s_d_h_p_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_mu_d_p_h_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_mu_d_h_p_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_lambda_d_p_h_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_lambda_d_h_p_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_cp_d_p_h_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_cp_d_h_p_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_cv_d_p_h_ph(double p, double h, double& r) const;
-    virtual EOS_Internal_Error compute_d_cv_d_h_p_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_rho_d_p_h_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_rho_d_h_p_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_u_d_p_h_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_u_d_h_p_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_s_d_p_h_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_s_d_h_p_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_mu_d_p_h_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_mu_d_h_p_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_lambda_d_p_h_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_lambda_d_h_p_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_cp_d_p_h_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_cp_d_h_p_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_cv_d_p_h_ph(double p, double h, double& r) const;
+    EOS_Internal_Error compute_d_cv_d_h_p_ph(double p, double h, double& r) const;
     */
 
     /* Partial derivatives (P,T) */
-    virtual EOS_Internal_Error compute_d_h_d_p_T_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_h_d_T_p_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_h_d_p_T_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_h_d_T_p_pT(double p, double T, double& r) const;
 
     /*
      * Not used since too "touchy"
-    virtual EOS_Internal_Error compute_d_rho_d_p_T_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_rho_d_T_p_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_u_d_p_T_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_u_d_T_p_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_s_d_p_T_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_s_d_T_p_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_mu_d_p_T_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_mu_d_T_p_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_lambda_d_p_T_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_lambda_d_T_p_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_cp_d_p_T_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_cp_d_T_p_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_cv_d_p_T_pT(double p, double T, double& r) const;
-    virtual EOS_Internal_Error compute_d_cv_d_T_p_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_rho_d_p_T_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_rho_d_T_p_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_u_d_p_T_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_u_d_T_p_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_s_d_p_T_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_s_d_T_p_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_mu_d_p_T_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_mu_d_T_p_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_lambda_d_p_T_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_lambda_d_T_p_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_cp_d_p_T_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_cp_d_T_p_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_cv_d_p_T_pT(double p, double T, double& r) const;
+    EOS_Internal_Error compute_d_cv_d_T_p_pT(double p, double T, double& r) const;
     */
 
     //
@@ -276,26 +220,22 @@ namespace NEPTUNE_EOS
     const Type_Info& get_Type_Info () const;
 
     //Error metod
-    void describe_error(const EOS_Internal_Error error, AString & description) const;
+    void describe_error(const EOS_Internal_Error error, string & description) const;
 
 
   protected:
-    //! Abstract handle for low-level Coolprop API
-    long abstract_state_handle_;
-
     double sat_quality_;
     //! Handle phase id : phase_unkown, phase_gas or phase_liquid
-    AString handle_phase_;
+    string handle_phase_;
     //! Backend
-    AString backend_ ;
+    string backend_ ;
 
-    AString fluid_name_; //!< Name of fluid
-    AString fluid_name_phase_ ; //!< Name of fluid with phase
+    string fluid_name_; //!< Name of fluid
+    string fluid_name_phase_ ; //!< Name of fluid with phase
 
   private:
     double molar_mass_; // Molar mass (kg/mol)
     static int type_Id;
 
   };
-}
 #endif
